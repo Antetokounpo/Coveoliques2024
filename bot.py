b840a62ce959439ad716d096fd8075ae296cba6b
Alexandre Gobeil <alexgobheb@gmail.com>
2024-01-13 11:57:34 -0500

    radar in bot

--- bot.py
+++ bot.py
@@ -10,7 +10,10 @@ from math import degrees
 class Bot:
     def __init__(self):
         print("Initializing your super mega duper bot")
-        self.crew_member_assigned_to_cannon: Tuple[CrewMember, TurretStation] = None
+        self.crew_member_assigned_to_cannon: Tuple[CrewMember, TurretStation] =
+        self.current_index = 0  # To keep track of the current ship to be scanned
+        self.all_ships_scanned_once = False  # Flag to indicate if all ships have been scanned once
+        self.last_updated = 0;
 
     def distance_from_station(self, crew_member: CrewMember, station: Station) -> float:
         distances_from_stations = crew_member.distanceFromStations
@@ -20,6 +23,29 @@ class Bot:
         distance = [s for s in all_distances_from_stations if s.stationId == station.id]
 
         return distance[0].distance if distance else 99999
+
+    def scan_ships(self, game_message: GameMessage):
+        other_ships_ids = [shipId for shipId in game_message.shipsPositions.keys() if shipId != game_message.currentTeamId]
+        enemy_ships_info = {ship_id: [None, 0] for ship_id in self.other_ships_ids}
+        if self.all_ships_scanned_once:
+            return None  # Return None if all ships have been scanned once
+
+        ship_id_to_scan = self.other_ships_ids[self.current_index]
+        self.current_index += 1
+
+        if self.current_index >= len(self.other_ships_ids):
+            self.current_index = 0  # Reset index
+            self.all_ships_scanned_once = True  # Set flag to True after one full cycle
+            self.last_updated = 0
+
+        return ship_id_to_scan
+
+    def scan(self, actions):
+        to_scan = self.get_next_ship_to_scan()
+        if to_scan:
+            actions.append(RadarScanAction(self.get_next_ship_to_scan()))
+        else:
+            raise IndexError("Tous les vaisseaux ont ete scan")
 
     def init_crew_member_assigned_to_cannon(self, game_message):
         if self.crew_member_assigned_to_cannon is not None:
@@ -99,5 +125,7 @@ class Bot:
         print(game_message.constants.ship.stations.turretInfos)
         print(game_message.shipsPositions.get(other_ships_ids[0]))
         return actions
+
+
 if __name__ == "__main__":
     print(game_message.shipsPositions)
